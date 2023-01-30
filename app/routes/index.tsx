6c05f9231cff4f0fe5aca432df6631229b3ee629
fineboy <fineboy0407@gmail.com>
2023-01-30 14:58:33 +0800

    created the fist remix route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -63,6 +63,10 @@ export default function Index() {
           </div>
         </div>
 
+        <div className="mx-auto mt-16 max-w-7x1 text-center">
+          <Link to="/posts" className="text-1 text-blue-600 underline">Blog Posts</Link>
+        </div>
+
         <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[

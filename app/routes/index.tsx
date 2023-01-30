fc67c4515e6d95e97751027b76997574a4d61dbb
fineboy <fineboy0407@gmail.com>
2023-01-30 17:09:45 +0800

    Fetch data using remix loader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
             <div className="absolute inset-0">
               <img
                 className="h-full w-full object-cover"
-                src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
+                src=""
                 alt="Sonic Youth On Stage"
               />
               <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
